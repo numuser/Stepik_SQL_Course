77907352a6562464a730a48cf9f1c15ef4801856
Nikita V <vyatchanin.11@gmail.com>
2023-05-25 16:24:18 +0500

    Update SQL_limit_and_date_operations.sql

--- 1_base_of_sql/SQL_limit_and_date_operations.sql
+++ 1_base_of_sql/SQL_limit_and_date_operations.sql
@@ -35,9 +35,51 @@ ORDER BY count(city) DESC LIMIT 2;
 -- DATEDIFF('2020-05-09','2020-05-01')=8
 -- DATEDIFF(date_last, date_first)
 
-
 -- Вывести информацию о командировках во все города кроме Москвы и Санкт-Петербурга (фамилии и инициалы сотрудников, город,
 -- длительность командировки в днях, при этом первый и последний день относится к периоду командировки).
 -- Последний столбец назвать Длительность. Информацию вывести в упорядоченном по убыванию длительности поездки,
 -- а потом по убыванию названий городов (в обратном алфавитном порядке).
-SELECT name, city, 
+SELECT name, 
+       city, 
+       DATEDIFF(
+           date_last, date_first
+       ) + 1 as 'Длительность'
+FROM trip
+WHERE city NOT IN('Москва', 'Санкт-Петербург')
+ORDER BY Длительность DESC, city DESC;
+
+-- Вывести информацию о командировках сотрудника(ов), которые были самыми короткими по времени.
+SELECT name, city, date_first, date_last
+FROM trip
+WHERE DATEDIFF(date_last, date_first) = (
+    SELECT MIN(DATEDIFF(date_last, date_first)) 
+    FROM trip 
+    ORDER BY DATEDIFF(date_last, date_first) 
+    );
+
+-- Для того, чтобы выделить номер месяца из даты используется функция MONTH(дата).
+-- Например, 
+-- MONTH('2020-04-12') = 4.
+
+-- Вывести информацию о командировках, начало и конец которых относятся к одному месяцу (год может быть любой)
+SELECT name, city, date_first, date_last
+FROM trip
+WHERE MONTH(date_first) = MONTH(date_last)
+ORDER BY city, name;
+
+-- Вывести сумму суточных (произведение количества дней командировки и размера суточных) для командировок,
+-- первый день которых пришелся на февраль или март 2020 года.
+SELECT name, city, date_first,
+       (DATEDIFF(date_last, date_first) + 1) * per_diem as 'Сумма'
+FROM trip
+WHERE MONTHNAME(date_first) IN ('February', 'March')  
+ORDER BY name, Сумма DESC
+
+-- Вывести фамилию с инициалами и общую сумму суточных, полученных за все командировки для тех сотрудников,
+-- которые были в командировках больше чем 3 раза, в отсортированном по убыванию сумм суточных виде.
+SELECT name, 
+       SUM((DATEDIFF(date_last, date_first) + 1) * per_diem) AS 'Сумма'
+FROM trip
+GROUP BY name
+HAVING COUNT(name) > 3
+ORDER BY Сумма DESC
