b3d37d3ff925f0a2e494b86093bb6f92181b7158
Nikita V <vyatchanin.11@gmail.com>
2023-06-08 15:20:22 +0500

    Update SQL_joins.sql

--- 2_linked_table_queries/SQL_joins.sql
+++ 2_linked_table_queries/SQL_joins.sql
@@ -196,3 +196,56 @@ JOIN book ON author.author_id = book.author_id
 GROUP BY name_author
 HAVING COUNT(DISTINCT(genre_id)) = 1
 ORDER BY name_author
+
+-- Вывести информацию о книгах (название книги, фамилию и инициалы автора, название жанра, цену и количество экземпляров книг), написанных в самых популярных жанрах, 
+-- в отсортированном в алфавитном порядке по названию книг виде. Самым популярным считать жанр, общее количество экземпляров книг которого на складе максимально.
+SELECT title, name_author, name_genre, price, amount
+FROM 
+    author 
+    INNER JOIN book ON author.author_id = book.author_id
+    INNER JOIN genre ON  book.genre_id = genre.genre_id
+    WHERE genre.genre_id IN
+         (/* выбираем автора, если он пишет книги в самых популярных жанрах*/
+          SELECT query_in_1.genre_id
+          FROM 
+              ( /* выбираем код жанра и количество произведений, относящихся к нему */
+                SELECT genre_id, SUM(amount) AS sum_amount
+                FROM book
+                GROUP BY genre_id
+               )query_in_1
+          INNER JOIN 
+              ( /* выбираем запись, в которой указан код жанр с максимальным количеством книг */
+                SELECT genre_id, SUM(amount) AS sum_amount
+                FROM book
+                GROUP BY genre_id
+                ORDER BY sum_amount DESC
+                LIMIT 1
+               ) query_in_2
+          ON query_in_1.sum_amount= query_in_2.sum_amount
+         )
+ORDER BY title
+
+-- При описании соединения таблиц с помощью JOIN в некоторых случаях вместо ON и следующего за ним условия можно использовать оператор USING().
+-- USING позволяет указать набор столбцов, которые есть в обеих объединяемых таблицах.
+-- Пример
+-- Вывести название книг, фамилии и id их авторов.
+-- Вариант с ON
+-- SELECT title, name_author, author.author_id /* явно указать таблицу - обязательно */
+-- FROM 
+--     author INNER JOIN book
+--     ON author.author_id = book.author_id;
+
+-- Вариант с USING
+-- SELECT title, name_author, author_id /* имя таблицы, из которой берется author_id, указывать не обязательно*/
+-- FROM 
+--     author INNER JOIN book
+--     USING(author_id);
+
+-- Если в таблицах supply  и book есть одинаковые книги, которые имеют равную цену,  вывести их название и автора,
+-- а также посчитать общее количество экземпляров книг в таблицах supply и book,  столбцы назвать Название, Автор  и Количество.
+SELECT book.title AS 'Название', name_author AS 'Автор', book.amount + supply.amount AS 'Количество'
+FROM author
+JOIN book USING(author_id)
+JOIN supply ON book.title = supply.title 
+    AND author.name_author = supply.author
+    AND book.price = supply.price
