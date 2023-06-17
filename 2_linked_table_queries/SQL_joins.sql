7ef23201cbaa1438c0eaf1c57d3145296ac8842b
Nikita V <vyatchanin.11@gmail.com>
2023-06-17 13:38:47 +0500

    Update SQL_joins.sql

--- 2_linked_table_queries/SQL_joins.sql
+++ 2_linked_table_queries/SQL_joins.sql
@@ -249,3 +249,24 @@ JOIN book USING(author_id)
 JOIN supply ON book.title = supply.title 
     AND author.name_author = supply.author
     AND book.price = supply.price
+
+-- В таблице city для каждого города указано количество дней, за которые заказ может быть доставлен в этот город (рассматривается только этап "Транспортировка").
+-- Для тех заказов, которые прошли этап транспортировки, вывести количество дней за которое заказ реально доставлен в город.
+-- А также, если заказ доставлен с опозданием, указать количество дней задержки, в противном случае вывести 0.
+SELECT buy_id, DATEDIFF(date_step_end, date_step_beg) AS 'Количество_дней', IF(
+    DATEDIFF(date_step_end, date_step_beg) > days_delivery,
+    DATEDIFF(date_step_end, date_step_beg) - days_delivery, 0
+    ) AS 'Опоздание'
+FROM city
+    JOIN client USING(city_id)
+    JOIN buy USING(client_id)
+    JOIN buy_step USING(buy_id)
+    JOIN step USING(step_id)
+WHERE buy_step.step_id = 3
+        AND date_step_end IS NOT NULL
+ORDER BY buy_id;
+
+-- Выбрать всех клиентов, которые заказывали книги Достоевского, информацию вывести в отсортированном по алфавиту виде. В решении используйте фамилию автора, а не его id.
+
+
+
