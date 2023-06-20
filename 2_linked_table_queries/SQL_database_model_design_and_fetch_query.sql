907c7d16c701265a5d8d43f8fe20291eea68f149
Nikita V <vyatchanin.11@gmail.com>
2023-06-20 13:23:31 +0500

    Update SQL_database_model_design_and_fetch_query.sql

--- 2_linked_table_queries/SQL_database_model_design_and_fetch_query.sql
+++ 2_linked_table_queries/SQL_database_model_design_and_fetch_query.sql
@@ -12,7 +12,7 @@ ORDER BY buy_id, title;
 
 -- Посчитать, сколько раз была заказана каждая книга, для книги вывести ее автора.
 -- Вывести фамилию и инициалы автора, название книги, последний столбец назвать Количество. Результат отсортировать сначала  по фамилиям авторов, а потом по названиям книг.
-SELECT name_author, title, COUNT(buy_book.amount) AS Количество
+SELECT name_author, title, COUNT(buy_book.amount) AS 'Количество'
 FROM author
     JOIN book ON author.author_id = book.author_id
     LEFT JOIN buy_book ON book.book_id = buy_book.book_id
@@ -51,3 +51,78 @@ FROM step
     JOIN buy_step ON step.step_id = buy_step.step_id
 WHERE date_step_beg IS NOT NULL AND date_step_end IS NULL
 ORDER BY buy_id;
+
+-- Выбрать всех клиентов, которые заказывали книги Достоевского, информацию вывести в отсортированном по алфавиту виде. В решении используйте фамилию автора, а не его id.
+SELECT DISTINCT name_client
+FROM author
+    JOIN book USING(author_id)
+    JOIN buy_book USING(book_id)
+    JOIN buy USING(buy_id)
+    JOIN client USING(client_id)
+WHERE author.name_author = 'Достоевский Ф.М.'
+ORDER BY name_client;
+
+-- Вывести жанр (или жанры), в котором было заказано больше всего экземпляров книг, указать это количество . Последний столбец назвать Количество.
+SELECT name_genre, SUM(buy_book.amount) AS 'Количество'
+FROM genre
+    JOIN book USING(genre_id)
+    JOIN buy_book USING(book_id)
+GROUP BY name_genre
+HAVING SUM(buy_book.amount) = 
+     (/* вычисляем максимальное из общего количества книг каждого жанра */
+      SELECT MAX(sum_amount) AS max_sum_amount
+      FROM 
+          (/* считаем количество книг каждого жанра */
+            SELECT genre_id, SUM(buy_book.amount) AS sum_amount 
+            FROM book
+                JOIN buy_book USING(book_id) 
+            GROUP BY genre_id
+          ) query_in
+      );
+
+------------------------------------------------- UNION and UNION ALL --------------------------------------------------------------------------
+-- Оператор UNION
+-- Оператор UNION используется для объединения двух и более SQL запросов, его синтаксис:
+
+-- SELECT столбец_1_1, столбец_1_2, ...
+-- FROM 
+--   ...
+-- UNION
+-- SELECT столбец_2_1, столбец_2_2, ...
+-- FROM 
+--   ...
+-- или
+-- SELECT столбец_1_1, столбец_1_2, ...
+-- FROM 
+--   ...
+-- UNION ALL
+-- SELECT столбец_2_1, столбец_2_2, ...
+-- FROM 
+--   ...
+-- Важно отметить, что каждый из SELECT должен иметь в своем запросе одинаковое количество столбцов и  совместимые типы возвращаемых данных. Каждый запрос может включать разделы WHERE, GROUP BY и пр.
+-- В результате выполнения этой конструкции будет выведена таблица, имена столбцов которой соответствуют именам столбцов в первом запросе. А в таблице результата сначала отображаются записи-результаты первого запроса, 
+-- а затем второго. Если указано ключевое слово ALL, то в результат включаются все записи запросов, в противном случае - различные.
+
+------------------------------------------------------------------------------------------------------------------------------------------------
+
+-- Сравнить ежемесячную выручку от продажи книг за текущий и предыдущий годы. Для этого вывести год, месяц, сумму выручки в отсортированном сначала по возрастанию месяцев, затем по возрастанию лет виде.
+-- Название столбцов: Год, Месяц, Сумма.
+
+SELECT YEAR(date_payment) AS 'Год',
+       MONTHNAME(date_payment) AS 'Месяц',
+       SUM(amount * price) AS 'Сумма'
+FROM buy_archive
+GROUP BY Год, Месяц
+UNION ALL
+SELECT YEAR(date_step_end) AS 'Год',
+       MONTHNAME(date_step_end) AS 'Месяц', 
+       SUM(buy_book.amount * book.price) AS 'Сумма'
+FROM book 
+    INNER JOIN buy_book USING(book_id)
+    INNER JOIN buy USING(buy_id) 
+    INNER JOIN buy_step USING(buy_id)
+    INNER JOIN step USING(step_id) 
+WHERE buy_step.step_id = 1 AND date_step_end IS NOT NULL
+GROUP BY Год, Месяц
+ORDER BY Месяц, Год;
+
