e3cd648165e0f70bf4d5e162477a57aa9a325f3a
Nikita V <vyatchanin.11@gmail.com>
2023-06-20 16:29:27 +0500

    add 2.5 chapter

--- 2_linked_table_queries/SQL_DB_Inter_correct_qvery.sql
+++ 2_linked_table_queries/SQL_DB_Inter_correct_qvery.sql
@@ -1,7 +1,10 @@
 -- 2.5
 -- Включить нового человека в таблицу с клиентами. Его имя Попов Илья, его email popov@test, проживает он в Москве.
-INSERT INTO name_client, city_id, email
-SELECT 'Попов Илья', 1, 'popov@test'
-FROM city
-WHERE name_city = 'Москва';
-SELECT * FROM city;
+INSERT INTO client (name_client, city_id, email)
+SELECT DISTINCT 'Попов Илья', city_id, 'popov@test'
+FROM client
+WHERE city_id = (
+    SELECT city_id
+    FROM city
+    WHERE name_city = 'Москва'
+                );
